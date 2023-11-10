fccd4df10a0bc3cc50957ae35780c449d17f938d
dekangcao <cdk163youxiang@163.com>
2023-11-10 17:14:59 -0600

    sierpinski

--- src/main/racket/sierpinski/sierpinski.rkt
+++ src/main/racket/sierpinski/sierpinski.rkt
@@ -5,11 +5,26 @@
 
 ; Dekang Cao
 (define (sierpinski-triangle side-length n)
-        (error 'not-yet-implemented)) 
+(if (= n 0)
+  (triangle side-length 'solid 'blue)
+  (let* ((t2 (sierpinski-triangle (/ side-length 2) (- n 1)))
+        (top t2)
+        (left t2)
+        (right t2)
+        (bottom (beside left right )))(above top bottom))))
 
+; Define a function to create a Sierpiński Carpet
+(define (square side-length color)
+(rectangle side-length side-length 'solid color))
 (define (sierpinski-carpet side-length n)
-        (error 'not-yet-implemented)) 
-
+(if (= n 0)
+  (square side-length "orange")
+  (let* ((c2 (/ side-length 3))
+      (c2c (sierpinski-carpet c2 (- n 1)))
+      (spacer (square c2 "white"))
+      (row (lambda (a b c) (beside a b c)))
+      (cr (row c2c c2c c2c))
+      (mr (row c2c spacer c2c)))(above cr mr cr))))
 
 (module+ main ; evualated when enclosing module is run directly (that is: not via require)
   (sierpinski-triangle 400 0)
@@ -24,3 +39,6 @@
   (sierpinski-carpet 400 3)
   (sierpinski-carpet 400 4)
 )
+
+
+

0720fa2f4afacf671e5a018eb5b375b3c27137c3
dekangcao <cdk163youxiang@163.com>
2023-11-10 19:53:52 -0600

    cantor

--- src/main/racket/cantor/cantor.rkt
+++ src/main/racket/cantor/cantor.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (require 2htdp/image)
+(require 2htdp/universe)
 (require "../spacer/spacer.rkt")
 (provide (all-defined-out))
 
@@ -11,8 +12,19 @@
   )
 
 ; Dekang Cao
+(define (white w h)
+(rectangle w h "solid" "white"))
 (define (cantor-stool width height n)
-        (error 'not-yet-implemented)) 
+(let* ((base (brick width (/ height 2)))
+  (space (white (/ width 3) (/ height 2))))
+  (if (= n 0)
+  (brick width height)
+    (let* ((neww (/ width 3))
+      (newh (/ height 2))
+      (left (cantor-stool neww newh (- n 1)))
+      (middle space)
+      (right (cantor-stool neww newh (- n 1)))
+      (top (beside left (beside middle right))))(above base top)))))
 
 (module+ main ; evualated when enclosing module is run directly (that is: not via require)
   (cantor-stool 400 400 0)
@@ -21,3 +33,4 @@
   (cantor-stool 400 400 3)
   (cantor-stool 400 400 4)
 )
+

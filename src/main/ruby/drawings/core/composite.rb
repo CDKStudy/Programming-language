38ca90b80f773368acaca0a6229ff4d52a5b0d39
dekangcao <cdk163youxiang@163.com>
2023-12-15 21:17:34 -0600

    render

--- src/main/ruby/drawings/core/composite.rb
+++ src/main/ruby/drawings/core/composite.rb
@@ -1,27 +1,38 @@
 # Dekang Cao
 
 module Composite
-  # Ensure this method is private
 
-  def initialize_components
-    @components = []
-  end
-
-  # Method to add a component to the composite
   def push_component(component)
     @components << component
   end
   private
+  def initialize_components
+    @components = []
+  end
   def untransformed_render(g)
     @components.each { |component| component.render(g) }
   end
-  # This method should be implemented by including classes
-  def untransformed_render
-    raise NotImplementedError, 'untransformed_render must be implemented'
-  end
 
-  # This method should be implemented by including classes
+
   def untransformed_bounds
-    raise NotImplementedError, 'untransformed_bounds must be implemented'
+    min_x = Float::INFINITY
+    min_y = Float::INFINITY
+    max_x = -Float::INFINITY
+    max_y = -Float::INFINITY
+
+    for component in @components
+      bounds = component.get_untransformed_bounds
+      new_min_x = bounds.min.x + component.x
+      new_min_y = bounds.min.y + component.y
+      new_max_x = bounds.max.x + component.x
+      new_max_y = bounds.max.y + component.y
+
+      min_x = [min_x, new_min_x].min
+      min_y = [min_y, new_min_y].min
+      max_x = [max_x, new_max_x].max
+      max_y = [max_y, new_max_y].max
+    end
+
+    BoundingBox.new(Point2.new(min_x, min_y), Point2.new(max_x, max_y))
   end
 end
--- src/main/ruby/drawings/core/scene.rb
+++ src/main/ruby/drawings/core/scene.rb
@@ -12,9 +12,6 @@ class Scene
      initialize_components
   end
 
-  def push_component(component)
-    @components << component
-  end
 
   def render(g)
     g.clear(@background_color)
@@ -23,18 +20,25 @@ class Scene
     end
   end
   def bounds
-    min_point = Point2.new(Float::INFINITY, Float::INFINITY)
-    max_point = Point2.new(-Float::INFINITY, -Float::INFINITY)
+    min_x = Float::INFINITY
+    min_y = Float::INFINITY
+    max_x = -Float::INFINITY
+    max_y = -Float::INFINITY
 
-    @components.each do |component|
-      comp_bounds = component.bounds
-      min_point.x = [min_point.x, comp_bounds.min.x].min
-      min_point.y = [min_point.y, comp_bounds.min.y].min
-      max_point.x = [max_point.x, comp_bounds.max.x].max
-      max_point.y = [max_point.y, comp_bounds.max.y].max
+    for component in @components
+      bounds = component.get_untransformed_bounds
+      new_min_x = bounds.min.x + component.x
+      new_min_y = bounds.min.y + component.y
+      new_max_x = bounds.max.x + component.x
+      new_max_y = bounds.max.y + component.y
+
+      min_x = [min_x, new_min_x].min
+      min_y = [min_y, new_min_y].min
+      max_x = [max_x, new_max_x].max
+      max_y = [max_y, new_max_y].max
     end
 
-    BoundingBox.new(min_point, max_point)
+    BoundingBox.new(Point2.new(min_x, min_y), Point2.new(max_x, max_y))
   end
 end
 
